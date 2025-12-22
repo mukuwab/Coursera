2382298628148a406851f6a5ae0fb30d7e5d7da0
mukuwab <mukuwab@yahoo.com>
2025-12-22 11:46:01 -0600

    changes

---
+++ Meta_React_Basics/Labs/creating-a-route/src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import Homepage from "./Homepage";
+import AboutLittleLemon from "./AboutLittleLemon";
+import Contact from "./Contact";
+import { Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div> 
+	  <nav>
+      <Link to="/" className="nav-item">Homepage</Link>
+      <Link to="/about" className="nav-item">About Little Lemon</Link>
+      <Link to="/Contact" className="nav-item">Contact</Link>
+	  </nav>
+      <Routes> 
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/about" element={<AboutLittleLemon />}></Route>
+        <Route path="/Contact" element={<Contact/>}></Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
